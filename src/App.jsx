c2ac068da7bae026ecc1cef246160125768339e6
israelmitolu <israelmitolu@gmail.com>
2023-06-01 10:51:47 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import SpotifyNowPlaying from "./NowPlaying";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen object-fill backdrop-blur-xl ">
+      <SpotifyNowPlaying />
+    </div>
+  );
+}
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mint: "#dbfff8",
+        black: "#000000",
+        "gray-dark": "#242423",
+        "gray-light": "#525453",
+        white: "#EFF2F4",
+      },
+      fontSize: {
+        xs: "0.5rem",
+        sm: "1rem",
+        base: "16px",
+        "1xl": "1.5rem",
+        "2xl": "2rem",
+        "3xl": "5rem",
+      },
+      animation: {
+        "spin-slow": "spin 2s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
