e68ea58bc294d6b426506aed88d96c8a1459376a
israelmitolu <israelmitolu@gmail.com>
2023-06-03 16:20:03 +0100

    Add the UI for Last played item

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,122 @@
+import { useEffect, useState } from "react";
+
 import SpotifyNowPlaying from "./NowPlaying";
+import getNowPlayingItem from "./SpotifyAPI";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [loading, setLoading] = useState(true);
+  const [currentTrack, setcurrentTrack] = useState({});
+  const [lastPlayed, setLastPlayed] = useState({});
+
+  // Get the currently playing song from the Spotify API
+  useEffect(() => {
+    getNowPlayingItem().then((res) => {
+      setcurrentTrack(res);
+      setLoading(false);
+    });
+  });
+
+  // Get the last played song from the Spotify API
+  useEffect(() => {
+    getNowPlayingItem().then((res) => {
+      setLastPlayed(res);
+    });
+  }, []);
+
+  // Set the title of the page to the song title
+  useEffect(() => {
+    if (currentTrack && !loading) {
+      if (currentTrack.isPlaying) {
+        document.title = `Mitolu is listening to ${currentTrack.title}`;
+      } else {
+        document.title = `Mitolu last listened to ${currentTrack.title} by ${currentTrack.artist}`;
+      }
+    } else {
+      document.title = "Mitolu is offline";
+    }
+  }, [currentTrack, loading]);
+
+  // Set the background color of the page to the album cover
+  useEffect(() => {
+    if (currentTrack.albumImageUrl && currentTrack.isPlaying) {
+      document.body.style.backgroundImage = `url(${currentTrack.albumImageUrl})`;
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundBlendMode = "soft-light";
+    } else {
+      document.body.style.backgroundImage = "none";
+    }
+  }, [currentTrack.albumImageUrl, currentTrack.isPlaying]);
+
+  // Update the time every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // Format the time to be displayed
+  const formatTime = (time) => {
+    const timeString = time.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    const [hours, minutes] = timeString.split(":");
+
+    return (
+      <div className="time">
+        <span>{hours}</span>
+        <span className="animate-blink-time">:</span>
+        <span>{minutes}</span>
+      </div>
+    );
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen object-fill backdrop-blur-xl ">
-      <SpotifyNowPlaying />
+    <div className="flex flex-col items-center justify-between h-screen w-full p-6 m-auto backdrop-blur-xl">
+      <nav className="w-full font-medium text-white text-[0.85rem] uppercase flex justify-between items-center leading-4">
+        <p>
+          Israel <br /> Mitolu
+        </p>
+        <div className="text-black">
+          {!currentTrack.isPlaying ? (
+            <span className="bg-gray-100 w-fit py-[2px] px-[6px] rounded-[4px]">
+              Offline
+            </span>
+          ) : (
+            <div className="flex items-center justify-around bg-gray-100 w-fit py-[2px] px-2 rounded-[4px]">
+              <span>Live</span>
+              <div className="h-2 w-2 ml-1 rounded-full bg-red-700 animate-pulse"></div>
+            </div>
+          )}
+        </div>
+        <p className="text-right uppercase">
+          <span className="text-gray-300 text-[0.75rem]">Osun, NG</span>
+          <span>{formatTime(currentTime)}</span>
+        </p>
+      </nav>
+
+      <SpotifyNowPlaying
+        currentTrack={currentTrack}
+        loading={loading}
+        lastPlayed={lastPlayed}
+      />
+
+      <footer className="text-center text-gray-500 text-[0.75rem]">
+        <p>
+          Songs I&apos;m listening to on{" "}
+          <a
+            href="https://open.spotify.com/user/utawon8gsgty2atyw86cw94ym"
+            target="_blank"
+            rel="noreferrer"
+            className="text-gray-400 hover:text-gray-300"
+          >
+            Spotify{" "}
+          </a>
+          :)
+        </p>
+      </footer>
     </div>
   );
 }
--- src/NowPlaying.jsx
+++ src/NowPlaying.jsx
@@ -1,95 +1,61 @@
 /* eslint-disable react/prop-types */
 import spotifyLogo from "./assets/spotifyLogo.png";
-import { useEffect, useState } from "react";
-import getNowPlayingItem from "./SpotifyAPI";
 
-const SpotifyNowPlaying = (props) => {
-  const [loading, setLoading] = useState(true);
-  const [result, setResult] = useState({});
-
-  useEffect(() => {
-    Promise.all([
-      getNowPlayingItem(
-        props.client_id,
-        props.client_secret,
-        props.refresh_token
-      ),
-    ]).then((results) => {
-      setResult(results[0]);
-      setLoading(false);
-    });
-  });
-
-  // Set the title of the page to the song title
-  useEffect(() => {
-    if (result && !loading) {
-      if (result.isPlaying) {
-        document.title = `Mitolu is listening to ${result.title}`;
-      } else {
-        document.title = `Mitolu last listened to ${result.title} by ${result.artist}`;
-      }
-    } else {
-      document.title = "Mitolu is offline";
-    }
-  }, [result, loading]);
-
-  // Set the background color of the page to the album cover
-  useEffect(() => {
-    if (result.albumImageUrl && result.isPlaying) {
-      document.body.style.backgroundImage = `url(${result.albumImageUrl})`;
-      document.body.style.backgroundSize = "cover";
-      document.body.style.backgroundBlendMode = "soft-light";
-    } else {
-      document.body.style.backgroundImage = "none";
-    }
-  }, [result.albumImageUrl, result.isPlaying]);
-
+const SpotifyNowPlaying = ({ currentTrack, loading, lastPlayed }) => {
   return (
-    <div
-      className="flex
-         min-w-[200px] w-[300px] h-fit text-white items-center justify-center p-2  text-base border rounded-lg border-gray-dark bg-gray-dark"
-    >
+    <div className="flex min-w-[200px] w-[300px] h-auto text-white items-center justify-center p-2 text-base rounded-lg bg-white bg-opacity-20 backdrop-blur-xl">
       {loading ? (
         <div className="flex p-2 w-fit items-center justify-center text-gray text-base">
           <img className="w-14 h-14" src={spotifyLogo} alt="" />
           <p className="tracking-wider px-1 text-base text-left ">Loading...</p>
         </div>
       ) : (
-        <div className="flex p-2 w-fit text-base h-full items-center justify-center hover:cursor-pointer">
-          {result.isPlaying ? (
-            <div className=" w-fit  h-full py-2 flex flex-col group text-left justify-around">
-              <div className="w-full h-fit relative group-hover:scale-105 transition">
-                <img
-                  className="w-fit h-6 p-1 object-contain absolute z-10 animate-spin-slow"
-                  src={spotifyLogo}
-                  alt=""
-                />
+        <div className="flex p-2 w-fit text-base h-full items-center justify-center">
+          <div className=" w-fit  h-full py-2 flex flex-col group text-left justify-around">
+            <div className="w-full h-fit relative group-hover:scale-105 transition hover:cursor-pointer">
+              <img
+                className={`w-fit h-6 p-1 object-contain absolute z-10 ${
+                  currentTrack.isPlaying ? "animate-spin-slow" : ""
+                }`}
+                src={spotifyLogo}
+                alt=""
+              />
+              <a
+                href={
+                  currentTrack.isPlaying
+                    ? currentTrack.songUrl
+                    : lastPlayed.songUrl
+                }
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img
                   className="w-fit h-full rounded-lg"
-                  src={result.albumImageUrl}
-                  alt="album-image"
+                  src={
+                    currentTrack.isPlaying
+                      ? currentTrack.albumImageUrl
+                      : lastPlayed.albumImageUrl
+                  }
+                  alt={
+                    currentTrack.isPlaying
+                      ? currentTrack.title
+                      : lastPlayed.title
+                  }
                 />
-              </div>
+              </a>
+            </div>
 
-              <div className="flex flex-col uppercase tracking-tighter text-center mt-2">
-                <p className="text-[14px]">{result.artist}</p>
-                <a
-                  className="hover:underline transition text-2xl font-semibold"
-                  href={result.songUrl}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {result.title}
-                </a>
-              </div>
+            <div className="flex flex-col uppercase tracking-tighter text-center mt-2">
+              <p className="text-[14px]">
+                {currentTrack.isPlaying
+                  ? currentTrack.artist
+                  : lastPlayed.artist}
+              </p>
+              <p className="text-2xl font-medium uppercase">
+                {currentTrack.isPlaying ? currentTrack.title : lastPlayed.title}
+              </p>
             </div>
-          ) : (
-            // if not playing
-            <div className="flex items-center justify-around  uppercase text-center mt-2 ">
-              <img className="w-14 h-14 mr-6" src={spotifyLogo} alt="" />
-              <p>Not Playing</p>
-            </div>
-          )}
+          </div>
         </div>
       )}
     </div>
